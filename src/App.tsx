b50fccc7cbc79e2982661f82ed11e8d1d58a14ff
katllv <katharinavu@hotmail.com>
2025-10-03 19:34:01 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <div className="p-8">
+      <h1 className="mb-4">Sustainability Canvas</h1>
+      <Button>Test Button</Button>
+      <Button variant="outline" className="ml-4">Outline Button</Button>
+      <Button variant="ghost" className="ml-4">Ghost Button</Button>
+
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+import tailwindcssAnimate from 'tailwindcss-animate'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  plugins: [tailwindcssAnimate],
+}
